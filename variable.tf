bc2287b54ad79517086f8b3ca57f34559ac60d81
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-21 21:41:10 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
+# information needed for terraform connect to cloud provider azure
 variable "subscription_id"{
   type=string
 }
@@ -9,4 +10,44 @@ variable "client_secret"{
 }
 variable "tenant_id"{
   type=string
+}
+
+# Variables for lookup practice
+# -------------------
+variable "env_settings" {
+  description = "Map of environment -> instance type"
+  default = {
+    dev   = "t2.micro"
+    qa    = "t3.small"
+    stage = "t3.medium"
+    prod  = "m5.large"
+  }
+}
+ 
+variable "city_codes" {
+  description = "Map of city -> airport IATA code"
+  default = {
+    Vancouver = "YVR"
+    Toronto   = "YYZ"
+    Montreal  = "YUL"
+    Calgary   = "YYC"
+  }
+}
+ 
+variable "country_currency" {
+  description = "Map of country -> currency code"
+  default = {
+    Canada = "CAD"
+    USA    = "USD"
+    Mexico = "MXN"
+  }
+}
+ 
+variable "request_cities" {
+  description = "Cities to resolve into airport codes for exercise 8"
+  default     = ["Vancouver", "Calgary", "Ottawa"]
+}
+# A sentinel value used to detect missing keys via lookup() in exercise 9
+locals {
+  sentinel_missing = "__MISSING__"
 }
